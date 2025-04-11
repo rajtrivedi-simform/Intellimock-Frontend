893720ae802b241eb45dbd374673426ea99d0df2
Raj Trivedi <raj.trivedi@simformsolutions.com>
2025-04-11 18:35:19 +0530

    Feat: Integrated Backend'

--- src/app/components/Question/questionpage/questionpage.component.ts
+++ src/app/components/Question/questionpage/questionpage.component.ts
@@ -1,29 +1,29 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from '../../common/header/header.component';
 import { CommonModule } from '@angular/common';
 import { FetchQuestionService } from '../../../services/Question/fetch-question.service';
 import { Question } from '../../../constants/types';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-questionpage',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, RouterLink],
   templateUrl: './questionpage.component.html',
   styleUrl: './questionpage.component.css',
 })
 export class QuestionpageComponent {
   questions: Array<Question> = [];
   filteredQuestions: Array<Question> = [];
-  typeOptions = signal<Array<string>>([]);
-  selectedType: string = '';
+  typeOptions = ['Technical', 'HR', 'Coding'];
+  _selectedType: string = 'Technical';
 
-  constructor(private fetchQuestionService: FetchQuestionService) {
-    this.filterQuestions()
-  }
+  constructor(private fetchQuestionService: FetchQuestionService) {}
 
   ngOnInit() {
     this.fetchQuestionService.fetchQuestions().subscribe({
       next: (res) => {
         this.questions = res.data;
+        this.filterQuestions('');
       },
       error: (err) => {
         console.log(err);
@@ -31,14 +31,17 @@ export class QuestionpageComponent {
     });
   }
 
-  filterTypes() {}
+  filterQuestions(selectedType: string) {
+    if (selectedType == '') {
+      this.filteredQuestions = this.questions.filter(
+        (question) => question.type == this._selectedType
+      );
+    } else {
+      this.filteredQuestions = this.questions.filter((question) => question.type == selectedType);
+    }
+  }
 
-  filterQuestions(){
-    if(this.selectedType==""){
-      this.filteredQuestions = this.questions;
-    }
-    else{
-      this.filteredQuestions = this.questions.filter((question) => question.type == this.selectedType);
-    }
+  changeType(type: string) {
+    this._selectedType = type;
   }
 }
